d390dd5b6a12987b4f7862a31756e67b1e42e5f5
biluohc <biluohc@outlook.com>
2017-04-25 12:43:16 +0800

    fix build.rs about point_width on windows.

--- build.rs
+++ build.rs
@@ -1,17 +1,38 @@
-use std::process::Command;
-use std::env;
-
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-    println!("cargo:rustc-link-search=native={}", out_dir);
-    // Cargo build --release 把build.rs的输出当参数(link搜索路径,-vv可以看到)。
-    // Cargo build --release -vv 可以看到println的内容。。
-    if cfg!(target_os = "windows") {
+    platform::main();
+}
+
+#[cfg(unix)]
+mod platform {
+    pub fn main() {}
+}
+
+#[cfg(windows)]
+mod platform {
+    use std::process::Command;
+    use std::env;
+
+    pub fn main() {
+        let out_dir = env::var("OUT_DIR").unwrap();
+        println!("cargo:rustc-link-search=native={}", out_dir);
+        // Cargo build --release 把build.rs的输出当参数(link搜索路径,-vv可以看到)。
+        // Cargo build --release -vv 可以看到println的内容。。
         Command::new("windres")
             .arg("config/favicon.rc")
+            .arg("--target")
+            .arg(target())
             .arg("-o")
             .arg(&format!("{}/favicon.o", out_dir))
             .status()
             .unwrap();
+    }
+
+    // build can't use if cfg!(target-point-width="64"), instead by env::var()
+    fn target() -> &'static str {
+        if env::var("TARGET").unwrap().contains("x86_64") {
+            "pe-x86-64"
+        } else {
+            "pe-i386"
+        }
     }
 }
