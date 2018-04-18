8dc2eaf7d7bbed50ead0b6fb942c2d4332eb188c
biluohc <biluohc@qq.com>
2018-04-18 11:09:58 +0800

    add '/' for url_for_parent.

--- src/tools.rs
+++ src/tools.rs
@@ -1,7 +1,7 @@
 use url::percent_encoding::{percent_decode, percent_encode_byte};
 
 // 以后应该自己组结构体，Hyper 的 Url new方法都没, md 一个 path 都改不了，只能反复 decode..
-pub fn url_for_parent(path: &str) -> String{
+pub fn url_for_parent(path: &str) -> String {
     let req_path_dec = percent_decode(path.as_bytes()).decode_utf8().unwrap();
     let cow_str = req_path_dec.as_ref();
     let req_path_components = cow_str
@@ -9,7 +9,7 @@ pub fn url_for_parent(path: &str) -> String{
         .filter(|c| !c.is_empty())
         .collect::<Vec<_>>();
     // if `/` encoding as '%2F', brower will concat origin'path with req_path_parent
-    if req_path_components.len() > 1 {
+    let mut out = if req_path_components.len() > 1 {
         req_path_components[..req_path_components.len() - 1]
             .iter()
             .flat_map(|c| {
@@ -18,6 +18,8 @@ pub fn url_for_parent(path: &str) -> String{
             })
             .collect::<String>()
     } else {
-        "/".to_owned()
-    }
+        String::new()
+    };
+    out.push('/');
+    out
 }
